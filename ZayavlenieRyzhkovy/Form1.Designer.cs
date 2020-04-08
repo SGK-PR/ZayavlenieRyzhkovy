874dc1fcea24a747d90ff2ae05802dcaf98a57dc
Ryzhkovy <sgk-pr@mail.ru>
2020-04-08 10:54:29 +0300

    Добавьте файлы проекта.

---
+++ ZayavlenieRyzhkovy/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ZayavlenieRyzhkovy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbGroup = new System.Windows.Forms.ComboBox();
+            this.cbSurname = new System.Windows.Forms.ComboBox();
+            this.cbView = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbGroup
+            // 
+            this.cbGroup.FormattingEnabled = true;
+            this.cbGroup.Location = new System.Drawing.Point(60, 45);
+            this.cbGroup.Name = "cbGroup";
+            this.cbGroup.Size = new System.Drawing.Size(146, 21);
+            this.cbGroup.TabIndex = 0;
+            // 
+            // cbSurname
+            // 
+            this.cbSurname.FormattingEnabled = true;
+            this.cbSurname.Location = new System.Drawing.Point(253, 45);
+            this.cbSurname.Name = "cbSurname";
+            this.cbSurname.Size = new System.Drawing.Size(155, 21);
+            this.cbSurname.TabIndex = 1;
+            // 
+            // cbView
+            // 
+            this.cbView.FormattingEnabled = true;
+            this.cbView.Location = new System.Drawing.Point(451, 45);
+            this.cbView.Name = "cbView";
+            this.cbView.Size = new System.Drawing.Size(182, 21);
+            this.cbView.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(60, 103);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(174, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(253, 200);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(220, 33);
+            this.btOpen.TabIndex = 4;
+            this.btOpen.Text = "Получить документ";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cbView);
+            this.Controls.Add(this.cbSurname);
+            this.Controls.Add(this.cbGroup);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbGroup;
+        private System.Windows.Forms.ComboBox cbSurname;
+        private System.Windows.Forms.ComboBox cbView;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btOpen;
+    }
+}
+
---
+++ ZayavlenieRyzhkovy/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ap=Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Word;
+
+
+
+namespace ZayavlenieRyzhkovy
+{
+    public partial class Form1 : Form
+    {
+        private string[] vs = new string[] { "ИС-18-02" };
+        private string[] vs1 = new string[] { "Афанасьев А.", "Чиликин А.", "Хусаинов М." };
+        private string[] vs2 = new string[] { "Заявление" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbGroup.Items.AddRange(vs);
+            cbSurname.Items.AddRange(vs1);
+            cbView.Items.AddRange(vs2);
+
+        }
+        
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            Ap.Application app = new Ap.Application();
+            Document doc = app.Documents.Add(Visible: true);
+
+            Range r= doc.Range();
+            r.Text = "Директору ГАПОУ СГК\n"  + 
+                " О.Н.Шалдыбиной \n" +
+            "Студент группы "+" " + cbGroup.SelectedItem+"\n"
+            +    cbSurname.SelectedItem+"\n";
+            r.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
+            r.Font.Size = 14;
+            r.Font.Name = "Times New Roman";
+
+
+           Range d = doc.Range();
+            Object collapseDirection = Microsoft.Office.Interop.Word.WdCollapseDirection.wdCollapseEnd;
+            d.Collapse(ref collapseDirection);
+            d.InsertParagraphAfter();
+            d.Text = cbView.SelectedItem + "\n" + "Прошу назначить мне," +" "+ cbSurname.SelectedItem +" "+
+                ",стипендию в размере 100000 р.(Ста тысяч рублей) за то, что я такой хороший" + "\n";
+            d.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            d.Font.Size = 14;
+            d.Font.Name = "Times New Roman";
+
+
+
+            Range c = doc.Range();
+            Object collapseDirection1 = Microsoft.Office.Interop.Word.WdCollapseDirection.wdCollapseEnd;
+            c.Collapse(ref collapseDirection);
+            c.InsertParagraphAfter();
+            c.Text = cbSurname.SelectedItem + "\n" + dateTimePicker1.Text;
+            c.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
+            c.Font.Size = 14;
+            c.Font.Name = "Times New Roman";
+
+            doc.Save();
+         
+            try
+            {
+                doc.Close();
+                app.Quit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+    }
+}
